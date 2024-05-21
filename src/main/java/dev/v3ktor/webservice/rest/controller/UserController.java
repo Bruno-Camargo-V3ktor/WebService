379b83a080f5a2dc8bc7ef2397701627e661d244
Bruno C. <v3ktoryt@gmail.com>
2024-05-21 20:35:43 -0300

    Criando camada de service e o serviço da entidade 'User', tambem adicionando um novo Endpoint a controller de User para encontrar um User pelo id

--- src/main/java/dev/v3ktor/webservice/rest/controller/UserController.java
+++ src/main/java/dev/v3ktor/webservice/rest/controller/UserController.java
@@ -1,20 +1,33 @@
 package dev.v3ktor.webservice.rest.controller;
 
 import dev.v3ktor.webservice.model.entity.User;
+import dev.v3ktor.webservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController @RequestMapping("/api/users")
 public class UserController {
 
+    //ARIBUTOS
+    @Autowired
+    private UserService service;
+
     //ENDPOINTS
     @GetMapping()
-    public ResponseEntity<User> getAll()
+    public ResponseEntity< List<User> > getAll()
     {
-        User u = new User(0L, "Bruno C.", "brunocamargo2280@gmail", "11996489713", "brunobr11");
-        return ResponseEntity.ok(u);
+        return ResponseEntity.ok( service.getAll() );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById( @PathVariable("id") Long id )
+    {
+        return ResponseEntity.ok( service.getById(id) );
     }
 
 }
---
+++ src/main/java/dev/v3ktor/webservice/service/UserService.java
@@ -0,0 +1,32 @@
+package dev.v3ktor.webservice.service;
+
+import dev.v3ktor.webservice.model.entity.User;
+import dev.v3ktor.webservice.model.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    //ATRIBUTOS
+    @Autowired
+    private UserRepository userRepository;
+
+    //MÉTODOS
+    public List<User> getAll()
+    {
+        return userRepository.findAll();
+    }
+
+    public User getById(Long id)
+    {
+        User user = userRepository.findById(id)
+                .orElseThrow( EntityNotFoundException::new );
+
+        return user;
+    }
+
+}
